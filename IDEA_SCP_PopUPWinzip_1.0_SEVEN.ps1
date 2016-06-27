f15742d759ce83e3441b3a5c22941ea7651e81ea
GildasPR <gildas@preault.eu>
2016-06-27 15:19:40 +0200

    V1

---
+++ IDEA_SCP_PopUPWinzip_1.0_SEVEN.ps1
@@ -0,0 +1,32 @@
+﻿#------------------------------------------------------------------------------
+# V1.0 - 14/06/2016
+# Gildas PRÉAULT [AIS]
+#------------------------------------------------------------------------------
+#
+# Description : 
+# Ce script effetue les opérations suivantes :
+# 
+#       - Affichage d'une popup dans une TS SCCM
+
+function New-PopupMessage {
+
+$Message = "L'utilitaire de compression Winzip n'est pas présent!"
+$SecondsToWait = 300 
+$Title = "Alerte WINZIP"
+$Button = 0
+$Icon = 48
+
+# Creation du PopUp
+(New-Object -ComObject Wscript.Shell).popup($Message,$SecondsToWait,$Title,$Button + $Icon)
+}
+ 
+# Fermeture de la barre de progression de la TS 
+try
+    {
+        $TSProgressUI = New-Object -COMObject Microsoft.SMS.TSProgressUI
+        $TSProgressUI.CloseProgressDialog()
+    }
+Catch {}
+ 
+# appel de la popup
+New-PopupMessage
